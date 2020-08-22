907025ce362a04cad63697c27bb2dad3545b3dba
icweaver <iweaver@cfa.harvard.edu>
2020-08-21 21:58:52 -0400

    switched to static html

--- index_files/editor.js
+++
@@ -1,6 +0,0 @@
-import { html, render } from "./common/Preact.js"
-
-import { Editor } from "./components/Editor.js"
-
-// it's like a Rube Goldberg machine
-render(html`<${Editor} />`, document.body)
